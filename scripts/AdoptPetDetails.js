6d791fbace9e10240329aab5b2d456fdbf7b19d6
Flora Su <fsu8@my.bcit.ca>
2025-03-08 16:24:56 -0800

    modified 'URL' and 'contacts' button, 'contacts' button is now connected to firestore

--- scripts/AdoptPetDetails.js
+++ scripts/AdoptPetDetails.js
@@ -1,7 +1,5 @@
 function displayPetInfo() {
-  //obtain pet ID using URL
-  let params = new URL(window.location.href);
-  let ID = params.searchParams.get("docID");
+  let ID = getPetID();
 
   //obrain pet's info from database using pet ID
   db.collection("petProfiles")
@@ -32,10 +30,17 @@ function displayPetInfo() {
       setFavorite();
     });
   
+}
+displayPetInfo();
+
+//obtain pet ID using URL
+function getPetID() {
+  let params = new URL(window.location.href);
+  let ID = params.searchParams.get("docID");
   return ID;
 }
-let petID = displayPetInfo();
 
+//obtain user ID from current user's firebase authenication account.
 function getUserID() {
   const user = firebase.auth().currentUser;
   if (user !== null) {
@@ -43,30 +48,74 @@ function getUserID() {
   }
 }
 
+//asks user if they want to send contact request to pet's owner (after
+//they click on the message button)
+function viewContactPrompt() {
+  let cardTemplate = document.getElementById("contactTemplate");
 
-function viewContact() {
-  clearMenu();
-  let ownerName = "owner name";
-  document.getElementById("menuPlaceholder").innerHTML = `<h3>${ownerName}</h3>`;
-  document.getElementById("emailLink").innerHTML = 'Email';
-  document.getElementById("emailLink").href = emailLink;
-  document.getElementById("contactLink").innerHTML = "Add to Contacts";
+  //get the pet's name using petID
+  let petID = getPetID();
+  db.collection("petProfiles").doc(petID).get().then(doc => {
+    let name = doc.data().name;
+
+    //clear previous placeholder content and populate with template content
+    let newCard = cardTemplate.content.cloneNode(true);
+    newCard.getElementById("pet-name").innerHTML = name;
+    clearMenu();
+    document.getElementById("menuPlaceholder").appendChild(newCard);
+  });
+  
+}
+
+//sends contact request to pet's owner
+function sendRequest() {
+  let userID = getUserID();
+  let petID = getPetID();
+
+  db.collection("petProfiles").doc(petID).get().then(doc => {
+    //get pet info
+    let interestedList = doc.data().interested;
+    let petName = doc.data().name;
+
+    //sets message depending of whether the user has already sent a previous contact request
+    //if it is a new request, user's ID will also be written into pet's "interested" field
+    let message = "";
+    if (interestedList.includes(userID)) {
+      message = `You have already send a contact request to ${petName}'s owner`;
+    } else {
+      db.collection("petProfiles").doc(petID).update({
+        interested: firebase.firestore.FieldValue.arrayUnion(userID)
+      });
+      message = `A contact request has been sent to ${petName}'s owner`;
+    }
+
+    document.getElementById("menuPlaceholder").innerHTML = message;
+    document.getElementById("hidePlaceholder").innerHTML = "hide";
+  });
+
+}
+
+//adds a copy URL button when share button is clicked
+function viewURL() {
+  let message = `<input type="button" value="Copy URL" onclick="copyURL()">`;
+  document.getElementById("menuPlaceholder").innerHTML = message;
   document.getElementById("hidePlaceholder").innerHTML = "hide";
 }
 
-function viewURL() {
-  clearMenu();
-  let info = "<h4>URL</h4>";
-  document.getElementById("menuPlaceholder").innerHTML = info;
-  document.getElementById("hidePlaceholder").innerHTML = "hide";
+//copies URL of page to clipboard, with popup confirmation message
+function copyURL() {
+  let URLlink = new URL(window.location.href);
+  navigator.clipboard.writeText(URLlink);
+  alert("URL has been copied");
 }
 
 //toggles favorite button on/off when clicked, add/deletes petID from current user's 'favorites' field
 function changeFavorite() {
-  const ID = getUserID();
+  let userID = getUserID();
+  let petID = getPetID();
   
   //get user's 'favorites' field and store in variable favoriteList
-  let docRef = db.collection("userProfiles").doc(ID);
+  let docRef = db.collection("userProfiles").doc(userID);
   docRef.get().then(doc => {
     docInfo = doc.data();
     let favoriteList = doc.data().favorites;
@@ -89,11 +138,11 @@ function changeFavorite() {
   });
 }
 
-//sets the favorite icon (filled/unfilled) based on user's favorite field
+//sets the favorite icon (filled/unfilled) based on user's "favorite" field
 function setFavorite() {
-  const ID = getUserID();
-  let docRef = db.collection("userProfiles").doc(ID);
-  console.log(ID);
+  let userID = getUserID();
+  let petID = getPetID();
+  let docRef = db.collection("userProfiles").doc(userID);
   docRef.get().then(doc => {
     docInfo = doc.data();
     let favoriteList = doc.data().favorites;
@@ -105,7 +154,7 @@ function setFavorite() {
   });
 }
 
-//add "click" event listener to the hide buttons
+//add "click" event listener to the hide buttons to clear expandable content
 function addMenuListener(){
   document.getElementById("hidePlaceholder").addEventListener("click", () => {
   clearMenu();
@@ -113,11 +162,9 @@ function addMenuListener(){
 }
 addMenuListener();
 
-function clearMenu () {
+//removes expandable content
+function clearMenu() {
   document.getElementById("menuPlaceholder").innerHTML = "";
   document.getElementById("hidePlaceholder").innerHTML = "";
-  document.getElementById("emailLink").innerHTML = "";
-  document.getElementById("emailLink").href = "";
-  document.getElementById("contactLink").innerHTML = "";
 }
 
