1242376ab5cb3c6148388258b4ac98fea2b2ffbf
Joaquin <jparedes.peake@gmail.com>
2025-03-13 10:38:00 -0700

    Naming convention changed to cammel case to additional files

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -7,17 +7,17 @@ function loadSkeleton() {
       // If the "user" variable is not null, then someone is logged in
           // User is signed in.
           // Do something for the user here.
-          console.log($('#adoptNavbarPlaceholder').load('/html/Components/adoptNavBar.html'));
-          console.log($('#adoptFooterPlaceholder').load('/html/Components/adoptFooter.html'));
-          console.log($('#rehomeFooterPlaceholder').load('/html/Components/rehomeFooter.html'));
-          console.log($('#rehomeNavbarPlaceholder').load('/html/Components/rehomeNavBar.html'));
+          console.log($('#adoptNavbarPlaceholder').load('/html/components/adoptNavBar.html'));
+          console.log($('#adoptFooterPlaceholder').load('/html/components/adoptFooter.html'));
+          console.log($('#rehomeFooterPlaceholder').load('/html/components/rehomeFooter.html'));
+          console.log($('#rehomeNavbarPlaceholder').load('/html/components/rehomeNavBar.html'));
           //if (user.metadata.creationTime == user.metadata.lastSignInTime) {
           //  console.log("new user");
       } else {
-        console.log($('#adoptNavbarPlaceholder').load('/html/Components/loginNavBar.html'));
-        console.log($('#rehomeNavbarPlaceholder').load('/html/Components/loginNavBar.html'));
-        console.log($('#adoptFooterPlaceholder').load('/html/Components/laoptFooter.html'));
-        console.log($('#rehomeFooterPlaceholder').load('/html/Components/rehomeFooter.html'));
+        console.log($('#adoptNavbarPlaceholder').load('/html/components/loginNavBar.html'));
+        console.log($('#rehomeNavbarPlaceholder').load('/html/components/loginNavBar.html'));
+        console.log($('#adoptFooterPlaceholder').load('/html/components/laoptFooter.html'));
+        console.log($('#rehomeFooterPlaceholder').load('/html/components/rehomeFooter.html'));
       }
   });
 }
