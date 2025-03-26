36decf3272c67e63b5c8467945c6b2ed0f8e991b
Joaquin <jparedes8@my.bcit.ca>
2025-03-25 22:46:09 -0700

    adoptContacts page finished with full layout and back-end functions

--- scripts/adoptContacts.js
+++ scripts/adoptContacts.js
@@ -6,7 +6,7 @@ async function displayPetContacts() {
     .then(user => {
         var interested = user.data().interested;
 
-        console.log(interested);
+        var contacts = user.data().contacts;
 
         interested.forEach(inter => {
             if (inter != "") {
@@ -14,16 +14,44 @@ async function displayPetContacts() {
                 .then(doc => {
                     var petName = doc.data().name;
                     var petImage = doc.data().petCode;
-                    var owner = doc.data().ownerID;
 
                     var newcard = cardTemplate.content.cloneNode(true);
 
                     newcard.querySelector(".pet-image").src = "data:image/png;base64," + petImage;
                     newcard.querySelector(".pet-name").innerHTML = "NAME: " + petName;
+                    newcard.querySelector(".request-status").innerHTML = "REQUEST PENDING";
 
                     document.getElementById("contacts-go-here").appendChild(newcard);
                 })
             }
+        })
+
+        contacts.forEach(inter => {
+            if (inter != "") {
+                db.collection("petProfiles").doc(inter).get()
+                .then(doc => {
+                    var petName = doc.data().name;
+                    var petImage = doc.data().petCode;
+                    var owner = doc.data().ownerID;
+                    var ownerEmail;
+                    var ownerName;
+
+                    db.collection("userProfiles").doc(owner).get().then(petOwner => {
+                        ownerEmail = petOwner.data().email;
+                        ownerName = petOwner.data().name;
+
+                        var newcard = cardTemplate.content.cloneNode(true);
+
+                        newcard.querySelector(".pet-image").src = "data:image/png;base64," + petImage;
+                        newcard.querySelector(".pet-name").innerHTML = "NAME: " + petName;
+                        newcard.querySelector(".owner-email").innerHTML = `<a href="mailto:${ownerEmail}">${ownerEmail}</a>`;
+                        newcard.querySelector(".owner-name").innerHTML = "OWNER NAME: " + ownerName;
+
+                        document.getElementById("contacts-go-here").appendChild(newcard);
+                    })
+
+              })
+          }
         })
     })
 }
@@ -34,10 +62,8 @@ function getUserID() {
     return new Promise((resolve, reject) => {
       firebase.auth().onAuthStateChanged((user) => {
         if (user) {
-          console.log("User is logged in:", user.uid);
           resolve(user.uid);
         } else {
-          console.log("User is not logged in.");
           reject("No user is signed in");
           window.location = "/html/login.html";
         }
